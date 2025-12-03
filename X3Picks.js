a3ab8aaf495c624baed92153d0e5b75b3bbf84fb
Santiago Gaitan <109171065+sannngaitan@users.noreply.github.com>
2025-12-02 22:22:18 -0300

    Update X3Picks.js

--- X3Picks.js
+++ X3Picks.js
@@ -1,5 +1,18 @@
 const localStorage = require('./storage');
 
+const http = require('http');
+
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('X3 Host running\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`HTTP server listening on port ${PORT}`);
+});
+
 // HBINIT
 var room = HBInit({
     roomName: "🟣 X3 Thrivium (ELO + PICKS) 🟣",
